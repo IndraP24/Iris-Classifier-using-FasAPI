7a148d551330449db24243a66bb569259a313d21
IndraP24 <indrashis985@gmail.com>
2021-05-31 23:36:05 +0530

    Update host address and port numbers

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from router import iris_router
+import uvicorn
 
 app = FastAPI()
 app.include_router(iris_router.router, prefix='/iris')
@@ -8,3 +9,7 @@ app.include_router(iris_router.router, prefix='/iris')
 @app.get('/healthcheck', status_code=200)
 async def healthcheck():
     return 'Iris classifier is all ready to go!'
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='127.0.0.1', port=8000)
