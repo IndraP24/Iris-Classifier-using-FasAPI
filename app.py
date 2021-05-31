07f2481da3c191df0f18e03cb37d48ff4c6d9875
IndraP24 <indrashis985@gmail.com>
2021-05-31 22:37:08 +0530

    include healthcheck

--- app.py
+++ app.py
@@ -1,30 +1,10 @@
-# 1. Library Imports
-import uvicorn
 from fastapi import FastAPI
+from router import iris_router
 
-# 2. Create FastAPI app object
 app = FastAPI()
-
-# 3. Index route to open on localhost automatically
-@app.get('/')
-def index():
-    '''
-    This is an initial docstring. It is the default page that loads.
-    '''
-    return {'message': 'Hello, Stranger'}
+app.include_router(iris_router.router, prefix='/iris')
 
 
-# 4. Route with a single parameter, returns the parameter within a message
-
-@app.get('/{name}')
-def get_name(name: str):
-    '''
-    This is the second docstring. It loads when a parameter is provided.
-    '''
-    return {'message': f'Hello, {name}'}
-
-
-# 5. Run the API with uvicorn
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+@app.get('/healthcheck', status_code=200)
+async def healthcheck():
+    return 'Iris classifier is all ready to go!'
